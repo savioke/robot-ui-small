7a4272d1dc3dbc7ef835b2d44d6f464f28c82b2e
Trevor Thompson <trevorthompson@Trevors-MBP.home>
2023-11-14 08:13:28 -0800

    Rename topbar

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -74,7 +74,7 @@ export default function TopBar() {
         <Box sx={styles.metricContainer}>
           <Box sx={styles.rightContainer}>
             {displayState.connected ? <Wifi fontSize='large' /> : <WifiOff fontSize='large' />}
-            <Text sx={styles.robotNickname}>{displayState.hostname}</Text>
+            <Text sx={styles.robotNickname}>m1234</Text>
           </Box>
           <Divider
             flexItem
